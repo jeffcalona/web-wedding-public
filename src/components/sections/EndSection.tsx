3ff78c428d646150585bbbf6499e6a420d4727f1
jeffcalona <alohajeffo@gmail.com>
2024-11-08 17:46:01 +0100

    add evento solo para adultos

--- src/components/sections/EndSection.tsx
+++ src/components/sections/EndSection.tsx
@@ -17,7 +17,7 @@ const EndSection = () => {
                 </header>
             </div>
             <div className="absolute z-20">
-                <LargeP text="Para disfrutar de una velada especial, agradecemos que este sea un evento solo para adultos." textColor="text-white" />
+                <LargeP text="Para disfrutar de una velada especial, el evento será sólo para adultos." textColor="text-white" />
             </div>
             <div className="absolute z-20 bottom-36 w-full">
                 <LargeP text="confirma tu asistencia" textColor="text-white" />
